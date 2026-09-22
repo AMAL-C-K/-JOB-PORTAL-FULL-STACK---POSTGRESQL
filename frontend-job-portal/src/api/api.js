f6f3c24cf548485c3012c207d39377c524adf915
AMAL-C-K <amalckg1999@gmail.com>
2026-09-22 16:42:32 +0530

    Update api.js

--- frontend-job-portal/src/api/api.js
+++ frontend-job-portal/src/api/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://job-portal-backend-api-78w1.onrender.com/api";
+const BASE_URL = "https://job-portal-backend-fxf3.onrender.com/api";
 export const apiFetch = async (endpoint, options = {}) => {
   let accessToken = localStorage.getItem("access");
 
